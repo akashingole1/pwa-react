dc5fd7286a047983bab5779473fa9b9d1266a257
Akash Ingole <akash.i@cashify.in>
2021-08-27 11:20:51 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hey there! Whats up
         </a>
       </header>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,10 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Hey there! Whats up/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('sum', () => {
+  expect(1+1).toBe(2);
 });
